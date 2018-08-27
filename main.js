8ad2284ea0eeecc0656b967949ce82a1064ae2c1
Douglas G. Moore <doug@dglmoore.com>
2018-08-27 22:17:22 +0000

    Setup server to deliver static content
    All static content in the `frontend` directory is being
    served.
    
    The `/` route redirects to `dashboard.html`, but this will be
    changed lest we want the dashboard unobscured.

--- main.js
+++ main.js
@@ -1,7 +1,22 @@
 // Copyright 2018 Nicole M. Lozano. All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
+/*jslint es6 */
+const express = require('express');
+const path = require('path');
 
-(function() {
-	console.log("Hello from sRAT");
-}())
+(function () {
+    'use strict';
+
+    const static_path = path.join(__dirname + '/frontend');
+
+    var app = express();
+
+    app.use(express.static(static_path));
+
+    app.get('/', function (ignore, res) {
+        res.redirect('dashboard.html');
+    });
+
+    app.listen(8080);
+}());
