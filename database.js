e532da65786420cbf767a960b4d738b78dc2cc26
Douglas G. Moore <doug@dglmoore.com>
2019-02-03 13:52:23 -0700

    Update copyright information

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-// Copyright 2018 Nicole M. Lozano. All rights reserved.
+// Copyright 2018-2019 Chet Gassett. All rights reserved.
 // Use of this source code is governed by the MIT
 // license that can be found in the LICENSE file.
 /*jslint es6 */
